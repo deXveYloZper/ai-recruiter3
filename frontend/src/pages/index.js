a5619ef35abf3572b788f12b148ffa07b19ff37c
deXveYloZper <xyzdeveloper@outlook.com>
2024-04-19 22:28:20 +0200

    adding also frontend

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>AI Recruiter</title>
+        <meta name="description" content="AI-powered recruitment platform" />
+      </Head>
+
+      <main >
+        <h1 >
+          Welcome to AI Recruiter!
+        </h1>
+        <p c>
+          Connecting jobs and talent through AI.
+        </p>
+      </main>
+    </div>
+  )
+}
